228df90274af2294cc78eba97ad05d45d0a5a3af
“emoliie” <emilie.xu@outlook.com>
2024-03-28 19:47:22 +0100

    first commit

---
+++ php/homepage.html.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="../css/nav-style.css" />
+  <link rel="stylesheet" href="../css/style.css" />
+  <link rel="stylesheet" href="../css/footer-style.css" />
+</head>
+
+<body>
+
+  <?php
+  require '../html/navigation.html';
+  ?>
+
+  <section class="banner-section">
+    <h2>Lorem ipsum dolor sit amet</h2>
+    <button class="btn">Découvrir</button>
+  </section>
+
+  <section class="content-section">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur.
+    </p>
+  </section>
+
+  <?php
+  require '../html/footer.html';
+  ?>
+</body>
+<script src="../js/menu-burger.js"></script>
+<script src="https://kit.fontawesome.com/5a6fe80c93.js" crossorigin="anonymous"></script>
+
+</html>
